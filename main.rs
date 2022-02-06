254d09de2042e24caff76cc992d234bc9397aa1d
Daryl Manning <daryl@wakatara.com>
2022-02-06 23:15:06 +0800

    Init commit.

---
+++ main.rs
@@ -0,0 +1,14 @@
+// Blockchain is a vector of blocks
+pub struct App {
+    pub blocks: Vec,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    pub data: String,
+    pub nonce: u64,
+}
