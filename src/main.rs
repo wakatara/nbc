5d9f9ff95f66a1b355c45f0b36be6fbccd5aa4b6
Daryl Manning <daryl@wakatara.com>
2022-02-20 15:32:51 +0800

    Lots of bugfixes/typos. Also, remember to put mod p2p in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod p2p;
 use chrono::prelude::*;
 use libp2p::{
     core::upgrade,
@@ -19,116 +20,6 @@ use tokio::{
     time::sleep,
 };
 
-
-#[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
-
-    // Initialize keypair, libp2p transport, behaviour, and p2p Swarm
-    info!("Peer Id: {}", p2p::PEER_ID.clone());
-    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
-    let (init_sender, mut init_rcv) = mpsc::unbounded_channel();
-
-    let auth_keys = Keypair::::new()
-        .into_authentic(&p2p::KEYS)
-        .expect("can create auth keys");
-
-    let transp = TokioTcpConfig::new()
-        .upgrade(upgrade::Version::V1)
-        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
-        .multiplex(mplex::MplexConfig::new())
-        .boxed();
-
-    let behaviour = p2p::AppBehaviour::new(App::new(), response_sender, init_sender.clone()).await;
-
-    let mut swarm = SwarmBuilder::new(transp, behaviour, *p2p::PEER_ID)
-        .executor(Box::new(|fut| {
-            spawn(fut);
-        }))
-        .build();
-
-    // buffered reader on StdIn     
-    let mut stdin = BufReader::new(stdin()).lines();
-
-    Swarm::listen_on(
-        &mut swarm,
-        "/ip4/0.0.0.0/tcp/0"
-            .parse()
-            .expect("can get a local socket"),
-    )
-    .expect("swarm can be started");
-
-    // Spawn async coroutine: waits and sends init trigger on init channel
-    spawn(async move {
-        sleep(Duration::from_secs(1)).await;
-        info!("sending init event");
-        init_sender.send(true).expect("can send init event");
-    });
-
-    // user events, incoming and outgoing data
-    loop {
-        let evt = {
-            // Tokio's select! macro to race multiple async functions
-            select! {
-                line = stdin.next_line() => Some(p2p::EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
-                response = response_rcv.recv() => {
-                    Some(p2p::EventType::LocalChainResponse(response.expect("response exists")))
-                },
-                _init = init_rcv.recv() => {
-                    Some(p2p::EventType::Init)
-                }
-                event = swarm.select_next_some() => {
-                    info!("Unhandled Swarm Event: {:?}", event);
-                    None
-                },
-            }
-        };
-
-        if let Some(event) = evt {
-            match event {
-                p2p::EventType::Init => {
-                    let peers = p2p::get_list_peers(&swarm);
-                    swarm.behaviour_mut().app.genesis();
-
-                    info!("connected nodes: {}", peers.len());
-                    if !peers.is_empty() {
-                        let req = p2p::LocalChainRequest {
-                            from_peer_id: peers
-                                .iter()
-                                .last()
-                                .expect("at least one peer")
-                                .to_string(),
-                        };
-
-                        let json = serde_json::to_string(&req).expect("can jsonify request");
-                        swarm
-                            .behaviour_mut()
-                            .floodsub
-                            .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
-                    }
-                }
-                p2p::EventType::LocalChainResponse(resp) => {
-                    let json = serde_json::to_string(&resp).expect("can jsonify response");
-                    swarm
-                        .behaviour_mut()
-                        .floodsub
-                        .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
-                }
-                // Just 3 commands
-                // ls p - prints all peers
-                // ls c - prints the chain locally 
-                // create b $data makes a block
-                p2p::EventType::Input(line) => match line.as_str() {
-                    "ls p" => p2p::handle_print_peers(&swarm),
-                    cmd if cmd.starts_with("ls c") => p2p::handle_print_chain(&swarm),
-                    cmd if cmd.starts_with("create b") => p2p::handle_create_block(cmd, &mut swarm),
-                    _ => error!("unknown command"),
-                },
-            }
-        }
-    }
-
-
 // Mining scheme - increases by more lead zeros
 // Normally in non-naive impl would be set by network 
 // attrib off consensus algo for consistent time-to-complete
@@ -136,7 +27,7 @@ const DIFFICULTY_PREFIX: &str = "00";
 
 // Blockchain is a vector of blocks
 pub struct App {
-    pub blocks: Vec,
+    pub blocks: Vec<Block>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -230,22 +121,9 @@ impl App {
         true
     }
 
-    fn is_chain_valid(&self, chain: &[Block]) -> bool {
-        for i in 0..chain.len() {
-            if i == 0 {
-                continue;
-            }
-            let first = chain.get(i - 1).expect("has to exist");
-            let second = chain.get(i).expect("has to exist");
-            if !self.is_block_valid(second, first) {
-                return false;
-            }
-        }
-        true
-    }
 
     // Always choose longest valid chain from a node
-    fn choose_chain(&mut self, local: Vec, remote: Vec) -> Vec {
+    fn choose_chain(&mut self, local: Vec<Block>, remote: Vec<Block>) -> Vec<Block> {
         let is_local_valid = self.is_chain_valid(&local);
         let is_remote_valid = self.is_chain_valid(&remote);
 
@@ -279,6 +157,7 @@ impl Block {
             nonce,
         }
     }
+}
 
     fn mine_block(id: u64, timestamp: i64, previous_hash: &str, data: &str) -> (u64, String) {
         info!("mining block...");
@@ -304,6 +183,7 @@ impl Block {
         }
     }
 
+
     fn calculate_hash(id: u64, timestamp: i64, previous_hash: &str, data: &str, nonce: u64) -> Vec<u8> {
         let data = serde_json::json!({
             "id": id,
@@ -317,7 +197,114 @@ impl Block {
         hasher.finalize().as_slice().to_owned()
     }
 
-}
+    #[tokio::main]
+    async fn main() {
+        pretty_env_logger::init();
+    
+        // Initialize keypair, libp2p transport, behaviour, and p2p Swarm
+        info!("Peer Id: {}", p2p::PEER_ID.clone());
+        let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+        let (init_sender, mut init_rcv) = mpsc::unbounded_channel();
+    
+        let auth_keys = Keypair::<X25519Spec>::new()
+            .into_authentic(&p2p::KEYS)
+            .expect("can create auth keys");
+    
+        let transp = TokioTcpConfig::new()
+            .upgrade(upgrade::Version::V1)
+            .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
+            .multiplex(mplex::MplexConfig::new())
+            .boxed();
+    
+        let behaviour = p2p::AppBehaviour::new(App::new(), response_sender, init_sender.clone()).await;
+    
+        let mut swarm = SwarmBuilder::new(transp, behaviour, *p2p::PEER_ID)
+            .executor(Box::new(|fut| {
+                spawn(fut);
+            }))
+            .build();
+    
+        // buffered reader on StdIn     
+        let mut stdin = BufReader::new(stdin()).lines();
+    
+        Swarm::listen_on(
+            &mut swarm,
+            "/ip4/0.0.0.0/tcp/0"
+                .parse()
+                .expect("can get a local socket"),
+        )
+        .expect("swarm can be started");
+    
+        // Spawn async coroutine: waits and sends init trigger on init channel
+        spawn(async move {
+            sleep(Duration::from_secs(1)).await;
+            info!("sending init event");
+            init_sender.send(true).expect("can send init event");
+        });
+    
+        // user events, incoming and outgoing data
+        loop {
+            let evt = {
+                // Tokio's select! macro to race multiple async functions
+                select! {
+                    line = stdin.next_line() => Some(p2p::EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
+                    response = response_rcv.recv() => {
+                        Some(p2p::EventType::LocalChainResponse(response.expect("response exists")))
+                    },
+                    _init = init_rcv.recv() => {
+                        Some(p2p::EventType::Init)
+                    }
+                    event = swarm.select_next_some() => {
+                        info!("Unhandled Swarm Event: {:?}", event);
+                        None
+                    },
+                }
+            };
+    
+            if let Some(event) = evt {
+                match event {
+                    p2p::EventType::Init => {
+                        let peers = p2p::get_list_peers(&swarm);
+                        swarm.behaviour_mut().app.genesis();
+    
+                        info!("connected nodes: {}", peers.len());
+                        if !peers.is_empty() {
+                            let req = p2p::LocalChainRequest {
+                                from_peer_id: peers
+                                    .iter()
+                                    .last()
+                                    .expect("at least one peer")
+                                    .to_string(),
+                            };
+    
+                            let json = serde_json::to_string(&req).expect("can jsonify request");
+                            swarm
+                                .behaviour_mut()
+                                .floodsub
+                                .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
+                        }
+                    }
+                    p2p::EventType::LocalChainResponse(resp) => {
+                        let json = serde_json::to_string(&resp).expect("can jsonify response");
+                        swarm
+                            .behaviour_mut()
+                            .floodsub
+                            .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
+                    }
+                    // Just 3 commands
+                    // ls p - prints all peers
+                    // ls c - prints the chain locally 
+                    // create b $data makes a block
+                    p2p::EventType::Input(line) => match line.as_str() {
+                        "ls p" => p2p::handle_print_peers(&swarm),
+                        cmd if cmd.starts_with("ls c") => p2p::handle_print_chain(&swarm),
+                        cmd if cmd.starts_with("create b") => p2p::handle_create_block(cmd, &mut swarm),
+                        _ => error!("unknown command"),
+                    },
+                }
+            }
+        }
+    }
 
 
 
--- src/p2p.rs
+++ src/p2p.rs
@@ -1,12 +1,25 @@
-// The Keypair and peerID basically identify client on p2p network
-pub static KEYS: Lazy = Lazy::new(identity::Keypair::generate_ed25519);
-pub static PEER_ID: Lazy = Lazy::new(|| PeerId::from(KEYS.public()));
-pub static CHAIN_TOPIC: Lazy = Lazy::new(|| Topic::new("chains"));
-pub static BLOCK_TOPIC: Lazy = Lazy::new(|| Topic::new("blocks"));
+use super::{App, Block};
+use libp2p::{
+    floodsub::{Floodsub, FloodsubEvent, Topic},
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    swarm::{NetworkBehaviourEventProcess, Swarm},
+    NetworkBehaviour, PeerId,
+};
+use log::{error, info};
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+use tokio::sync::mpsc;
+
+pub static KEYS: Lazy<identity::Keypair> = Lazy::new(identity::Keypair::generate_ed25519);
+pub static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+pub static CHAIN_TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("chains"));
+pub static BLOCK_TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("blocks"));
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ChainResponse {
-    pub blocks: Vec,
+    pub blocks: Vec<Block>,
     pub receiver: String,
 }
 
@@ -21,16 +34,14 @@ pub enum EventType {
     Init,
 }
 
-// FloodSub good enough for naive implmentation. Inefficient and super noisy though.
-// TODO Implement Gossip protocol
 #[derive(NetworkBehaviour)]
 pub struct AppBehaviour {
     pub floodsub: Floodsub,
     pub mdns: Mdns,
     #[behaviour(ignore)]
-    pub response_sender: mpsc::UnboundedSender,
+    pub response_sender: mpsc::UnboundedSender<ChainResponse>,
     #[behaviour(ignore)]
-    pub init_sender: mpsc::UnboundedSender,
+    pub init_sender: mpsc::UnboundedSender<bool>,
     #[behaviour(ignore)]
     pub app: App,
 }
@@ -38,8 +49,8 @@ pub struct AppBehaviour {
 impl AppBehaviour {
     pub async fn new(
         app: App,
-        response_sender: mpsc::UnboundedSender,
-        init_sender: mpsc::UnboundedSender,
+        response_sender: mpsc::UnboundedSender<ChainResponse>,
+        init_sender: mpsc::UnboundedSender<bool>,
     ) -> Self {
         let mut behaviour = Self {
             app,
@@ -57,9 +68,38 @@ impl AppBehaviour {
     }
 }
 
+// incoming event handler
+impl NetworkBehaviourEventProcess<FloodsubEvent> for AppBehaviour {
+    fn inject_event(&mut self, event: FloodsubEvent) {
+        if let FloodsubEvent::Message(msg) = event {
+            if let Ok(resp) = serde_json::from_slice::<ChainResponse>(&msg.data) {
+                if resp.receiver == PEER_ID.to_string() {
+                    info!("Response from {}:", msg.source);
+                    resp.blocks.iter().for_each(|r| info!("{:?}", r));
+
+                    self.app.blocks = self.app.choose_chain(self.app.blocks.clone(), resp.blocks);
+                }
+            } else if let Ok(resp) = serde_json::from_slice::<LocalChainRequest>(&msg.data) {
+                info!("sending local chain to {}", msg.source.to_string());
+                let peer_id = resp.from_peer_id;
+                if PEER_ID.to_string() == peer_id {
+                    if let Err(e) = self.response_sender.send(ChainResponse {
+                        blocks: self.app.blocks.clone(),
+                        receiver: msg.source.to_string(),
+                    }) {
+                        error!("error sending response via channel, {}", e);
+                    }
+                }
+            } else if let Ok(block) = serde_json::from_slice::<Block>(&msg.data) {
+                info!("received new block from {}", msg.source.to_string());
+                self.app.try_add_block(block);
+            }
+        }
+    }
+}
+
 impl NetworkBehaviourEventProcess<MdnsEvent> for AppBehaviour {
     fn inject_event(&mut self, event: MdnsEvent) {
-        // only two MDNS event types: discovered or expired
         match event {
             MdnsEvent::Discovered(discovered_list) => {
                 for (peer, _addr) in discovered_list {
@@ -77,37 +117,46 @@ impl NetworkBehaviourEventProcess<MdnsEvent> for AppBehaviour {
     }
 }
 
-// Handler for Incoming Events
-impl NetworkBehaviourEventProcess for AppBehaviour {
-    fn inject_event(&mut self, event: FloodsubEvent) {
-        // does incoming meet expected data structures?
-        // if ChainResponse: got sent local bc by other node - if so attempt execute consensus
-        // if valid and longer than our chain, replace our chain
-        // if Block and valid, we add it to our local chain
-        if let FloodsubEvent::Message(msg) = event {
-            if let Ok(resp) = serde_json::from_slice::(&msg.data) {
-                if resp.receiver == PEER_ID.to_string() {
-                    info!("Response from {}:", msg.source);
-                    resp.blocks.iter().for_each(|r| info!("{:?}", r));
-
-                    self.app.blocks = self.app.choose_chain(self.app.blocks.clone(), resp.blocks);
-                }
-            } else if let Ok(resp) = serde_json::from_slice::(&msg.data) {
-                info!("sending local chain to {}", msg.source.to_string());
-                let peer_id = resp.from_peer_id;
-                if PEER_ID.to_string() == peer_id {
-                    if let Err(e) = self.response_sender.send(ChainResponse {
-                        blocks: self.app.blocks.clone(),
-                        receiver: msg.source.to_string(),
-                    }) {
-                        error!("error sending response via channel, {}", e);
-                    }
-                }
-            } else if let Ok(block) = serde_json::from_slice::(&msg.data) {
-                info!("received new block from {}", msg.source.to_string());
-                self.app.try_add_block(block);
-            }
-        }
+pub fn get_list_peers(swarm: &Swarm<AppBehaviour>) -> Vec<String> {
+    info!("Discovered Peers:");
+    let nodes = swarm.behaviour().mdns.discovered_nodes();
+    let mut unique_peers = HashSet::new();
+    for peer in nodes {
+        unique_peers.insert(peer);
     }
+    unique_peers.iter().map(|p| p.to_string()).collect()
 }
 
+pub fn handle_print_peers(swarm: &Swarm<AppBehaviour>) {
+    let peers = get_list_peers(swarm);
+    peers.iter().for_each(|p| info!("{}", p));
+}
+
+pub fn handle_print_chain(swarm: &Swarm<AppBehaviour>) {
+    info!("Local Blockchain:");
+    let pretty_json =
+        serde_json::to_string_pretty(&swarm.behaviour().app.blocks).expect("can jsonify blocks");
+    info!("{}", pretty_json);
+}
+
+pub fn handle_create_block(cmd: &str, swarm: &mut Swarm<AppBehaviour>) {
+    if let Some(data) = cmd.strip_prefix("create b") {
+        let behaviour = swarm.behaviour_mut();
+        let latest_block = behaviour
+            .app
+            .blocks
+            .last()
+            .expect("there is at least one block");
+        let block = Block::new(
+            latest_block.id + 1,
+            latest_block.hash.clone(),
+            data.to_owned(),
+        );
+        let json = serde_json::to_string(&block).expect("can jsonify request");
+        behaviour.app.blocks.push(block);
+        info!("broadcasting new block");
+        behaviour
+            .floodsub
+            .publish(BLOCK_TOPIC.clone(), json.as_bytes());
+    }
+}
